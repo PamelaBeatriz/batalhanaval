f7443faaa459b5d9b6ba941bb4834005eafa1769
thiagogenez <thiagogenez@0efd2736-2953-0410-b2c1-0b640bbdf8ce>
2008-08-13 01:18:24 +0000

    arrumando

--- trunk/Cliente/src/gui/TabuleiroDaCasa.java
+++ trunk/Cliente/src/gui/TabuleiroDaCasa.java
@@ -1,6 +1,8 @@
 package gui;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -10,6 +12,7 @@ import java.util.Vector;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
 
 import logica.PictureTabuleiro;
 import logica.TabuleiroLogico;
@@ -17,7 +20,7 @@ import logica.TabuleiroLogico;
 public class TabuleiroDaCasa extends JPanel {
 
 	private static final long serialVersionUID = 1L;
-	private TabuleiroLogico tabuleiroLogico = null;
+	private TabuleiroLogico tabuleiroLogico = null;  //  @jve:decl-index=0:
 
 	private ArrayList<PictureTabuleiro> picturesList = null;
 
@@ -33,6 +36,16 @@ public class TabuleiroDaCasa extends JPanel {
 
 	private boolean handlerdesligado;
 
+	private String nickName;  //  @jve:decl-index=0:
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
 	public TabuleiroLogico getTabuleiroLogico() {
 		return tabuleiroLogico;
 	}
@@ -44,8 +57,9 @@ public class TabuleiroDaCasa extends JPanel {
 	/**
 	 * This is the default constructor
 	 */
-	public TabuleiroDaCasa() {
+	public TabuleiroDaCasa(String nickName) {
 		super();
+		this.nickName = nickName;
 		initialize();
 	}
 
@@ -55,9 +69,16 @@ public class TabuleiroDaCasa extends JPanel {
 	 * @return void
 	 */
 	private void initialize() {
-		this.setSize(250, 250);
+		this.setSize(new Dimension(250, 250));
 		this.setLayout(null);
 		this.setBorder(BorderFactory.createLineBorder(new Color(0, 100, 90)));
+
+		this.setBorder(BorderFactory.createTitledBorder(null,
+				this.nickName, TitledBorder.CENTER,
+				TitledBorder.DEFAULT_POSITION, new Font("Dialog",
+						Font.BOLD, 12), new Color(51, 51, 51)));
+
+
 		this.tabuleiroLogico = new TabuleiroLogico();
 		this.picturesList = new ArrayList<PictureTabuleiro>();
 		this.pictureTabuleiro = new Vector<PictureTabuleiro>(5);
