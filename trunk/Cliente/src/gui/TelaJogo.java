5d5f329a7a5de4c809776006c26265e0db199731
chrisandrew87 <chrisandrew87@0efd2736-2953-0410-b2c1-0b640bbdf8ce>
2008-08-13 21:54:46 +0000

    adicionado scroollpanel no chat e outras pequenas modificações

--- trunk/Cliente/src/gui/TelaJogo.java
+++ trunk/Cliente/src/gui/TelaJogo.java
@@ -1,28 +1,32 @@
 package gui;
 
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 import java.net.Socket;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
 import javax.swing.KeyStroke;
 import javax.swing.border.TitledBorder;
 
 import logica.Cliente;
 import logica.DataOutput;
 import logica.Packet;
+
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.JScrollPane;
 
 public class TelaJogo extends JFrame {
 
@@ -42,10 +46,10 @@ public class TelaJogo extends JFrame {
 	private JMenuItem sobreJMenuItem = null;
 	private JPanel painelControleJPanel = null;
 	private Cliente client;
-	private JLabel tabuleiroCasaLabel = null;
 	private JTextArea chatTextArea = null;
 	private JTextField inputTextField = null;
 	private DataOutput output;
+	private JScrollPane chatScrollPane = null;
 
 	public String getNickName() {
 		return nickName;
@@ -58,16 +62,12 @@ public class TelaJogo extends JFrame {
 	/**
 	 * This is the default constructor
 	 */
-	public TelaJogo(Socket socket) {
-		super();
-		initialize();
-	}
-
-	public TelaJogo(String nickName, Cliente cliente) {
+	public TelaJogo(String nickName, Cliente client) {
 		super();
 		this.nickName = nickName;
 		this.client = client;
 		initialize();
+		this.output = new DataOutput(this.client);
 	}
 
 	/**
@@ -76,7 +76,7 @@ public class TelaJogo extends JFrame {
 	 * @return void
 	 */
 	private void initialize() {
-		this.setSize(721, 622);
+		this.setSize(738, 614);
 		this.setJMenuBar(getJJMenuBar());
 		this.setContentPane(getJContentPane());
 		this.setTitle("Batalha Naval - O Jogo");
@@ -115,8 +115,8 @@ public class TelaJogo extends JFrame {
 			chatJPanel.setBorder(BorderFactory.createTitledBorder(null, "Chat",
 					TitledBorder.CENTER, TitledBorder.DEFAULT_POSITION,
 					new Font("Dialog", Font.BOLD, 12), new Color(51, 51, 51)));
-			chatJPanel.add(getChatTextArea(), null);
 			chatJPanel.add(getInputTextField(), null);
+			chatJPanel.add(getChatScrollPane(), null);
 		}
 		return chatJPanel;
 	}
@@ -128,19 +128,15 @@ public class TelaJogo extends JFrame {
 	 */
 	private JPanel getTabuleiroPanel() {
 		if (tabuleiroPanel == null) {
-			tabuleiroCasaLabel = new JLabel();
-			tabuleiroCasaLabel.setBounds(new Rectangle(16, 276, 108, 16));
-			tabuleiroCasaLabel.setText("Capitão: " + this.nickName);
 			tabuleiroPanel = new JPanel();
 			tabuleiroPanel.setLayout(null);
-			tabuleiroPanel.setBounds(new Rectangle(5, 4, 686, 301));
+			tabuleiroPanel.setBounds(new Rectangle(5, 4, 686, 312));
 			tabuleiroPanel.setBorder(BorderFactory.createTitledBorder(null,
 					"Tabuleiros", TitledBorder.CENTER,
 					TitledBorder.DEFAULT_POSITION, new Font("Dialog",
 							Font.BOLD, 12), new Color(51, 51, 51)));
 			tabuleiroPanel.add(getCasaTabuleiroPanel(), null);
 			tabuleiroPanel.add(getAdversarioTabuleiroPanel(), null);
-			tabuleiroPanel.add(tabuleiroCasaLabel, null);
 		}
 		return tabuleiroPanel;
 	}
@@ -152,9 +148,13 @@ public class TelaJogo extends JFrame {
 	 */
 	private JPanel getCasaTabuleiroPanel() {
 		if (casaTabuleiroPanel == null) {
-			casaTabuleiroPanel = new TabuleiroDaCasa();
-			casaTabuleiroPanel.setLocation(new Point(9, 13));
-			casaTabuleiroPanel.setSize(new Dimension(250, 250));
+			casaTabuleiroPanel = new JPanel();
+			casaTabuleiroPanel.setLayout(null);
+			casaTabuleiroPanel.setBounds(new Rectangle(7, 14, 303, 285));
+			casaTabuleiroPanel.setBorder(BorderFactory.createTitledBorder(null,
+					this.nickName, TitledBorder.CENTER,
+					TitledBorder.DEFAULT_POSITION, new Font("Dialog",
+							Font.BOLD, 12), new Color(51, 51, 51)));
 		}
 		return casaTabuleiroPanel;
 	}
@@ -168,8 +168,7 @@ public class TelaJogo extends JFrame {
 		if (adversarioTabuleiroPanel == null) {
 			adversarioTabuleiroPanel = new JPanel();
 			adversarioTabuleiroPanel.setLayout(null);
-			adversarioTabuleiroPanel
-					.setBounds(new Rectangle(378, 15, 236, 227));
+			adversarioTabuleiroPanel.setBounds(new Rectangle(378, 15, 300, 283));
 			adversarioTabuleiroPanel.setBorder(BorderFactory
 					.createTitledBorder(null, "Adversário",
 							TitledBorder.CENTER, TitledBorder.DEFAULT_POSITION,
@@ -187,10 +186,8 @@ public class TelaJogo extends JFrame {
 	private JMenuBar getJJMenuBar() {
 		if (jJMenuBar == null) {
 			jJMenuBar = new JMenuBar();
-			jJMenuBar.setLocation(new Point(0, 0));
 			jJMenuBar.add(getFileJMenu());
 			jJMenuBar.add(getHelpJMenu());
-
 		}
 		return jJMenuBar;
 	}
@@ -260,7 +257,6 @@ public class TelaJogo extends JFrame {
 			helpJMenu = new JMenu();
 			helpJMenu.setText("Help");
 			helpJMenu.setMnemonic(KeyEvent.VK_H);
-			helpJMenu.setLocation(new Point(0, 0));
 			helpJMenu.add(getComoJogarJMenuItem());
 			helpJMenu.addSeparator();
 			helpJMenu.add(getSobreJMenuItem());
@@ -306,102 +302,103 @@ public class TelaJogo extends JFrame {
 	 */
 	private JPanel getPainelControleJPanel() {
 		if (painelControleJPanel == null) {
-			painelControleJPanel = new PainelControle();
-			painelControleJPanel.setSize(new Dimension(308, 254));
-			painelControleJPanel.setLocation(new Point(5, 310));
+			painelControleJPanel = new JPanel();
+			painelControleJPanel.setLayout(null);
+			painelControleJPanel.setBounds(new Rectangle(7, 326, 318, 211));
+			painelControleJPanel.setBorder(BorderFactory.createTitledBorder(null, "Navios", TitledBorder.CENTER, TitledBorder.DEFAULT_POSITION, new Font("Dialog", Font.BOLD, 12), new Color(51, 51, 51)));
 		}
 		return painelControleJPanel;
 	}
 
-	/*
-	 * private void initComponents(){ //Cria uma box, que é um gerenciador de
-	 * containeres. Neste caso criamos uma box horizontal, e todo container
-	 * adicionado à box //o será da esquerda para a direita. Para quem quiser
-	 * saber mais sobre Box (BoxLayout), indiquei o link do tutorial da Sun //na
-	 * descrição da classe. Box boxHorizontalCima = Box.createHorizontalBox();
-	 *
-	 * //Aqui criamos 4 box auxiliares. A box1CimaX é uma box horizontal que
-	 * adiciona o container com as letras e o tabuleiro do //jogador. Depois
-	 * "empacotamos" esta box na box1CimaY, junto com o container com os
-	 * números. O mesmo acontece com box2CimaX //e box2CimaY. Explicarei melhor
-	 * no decorrer do código ;-) Box box1CimaX = Box.createHorizontalBox(); Box
-	 * box2CimaX = Box.createHorizontalBox(); Box box1CimaY =
-	 * Box.createVerticalBox(); Box box2CimaY = Box.createVerticalBox();
-	 *
-	 *
-	 *
-	 * Dimension distPadraoX = new Dimension(10, 249); Dimension distPadraoY =
-	 * new Dimension(250,10);
-	 *
-	 * //Criamos os containeres com as letras e números. bottom1 é um container
-	 * que serve apenas para dar um espaço de preenchimento. Container letras =
-	 * criarVetorLetras(); Container numeros = criarVetorNumeros(); Container
-	 * bottom1 = new Container();
-	 *
-	 * bottom1.setPreferredSize(new Dimension(251,15));
-	 *
-	 * //Primeiro adicionamos um espaço de preenchimento na box horizontal, com
-	 * dimensão 10,252. Isto cria o espaço entre o lado //esquerdo do frame e as
-	 * letras box1CimaX.add(new
-	 * Box.Filler(distPadraoX,distPadraoX,distPadraoX)); //Adicionamos as letras
-	 * e o tabuleiro do jogador box1CimaX.add(letras); //
-	 * box1CimaX.add(meuTabuleiro);
-	 *
-	 * //Depois adicionamos um espaço de preenchimento similar à box horizontal,
-	 * mas agora na vertical. Isto cria o espaço entre o //a parte de cima do
-	 * frame e os números box1CimaY.add(new
-	 * Box.Filler(distPadraoY,distPadraoY,distPadraoY)); //Adicionamos os
-	 * números e a box horizontal, para empacotar tudo numa grande box
-	 * box1CimaY.add(numeros); box1CimaY.add(box1CimaX); //Adicionamos um espaço
-	 * de preenchimento também na vertical. Isto cria o espaço entre o tabuleiro
-	 * e o painel com os botões //(ou dados da rede). box1CimaY.add(new
-	 * Box.Filler(distPadraoY,distPadraoY,distPadraoY)); //Por fim, adicionamos
-	 * a box que contém o tabuleiro, letras e números na box horizontal de cima.
-	 * Entenda que isto funciona //basicamente como uma série de empacotamentos
-	 * de containeres. Até aqui, adicionamos apenas o tabuleiro do jogador
-	 * boxHorizontalCima.add(box1CimaY); boxHorizontalCima.add(new
-	 * Box.Filler(distPadraoX,distPadraoX,distPadraoX)); }
-	 */
+	private void initComponents(){
+		//Cria uma box, que é um gerenciador de containeres. Neste caso criamos uma box horizontal, e todo container adicionado à box
+        //o será da esquerda para a direita. Para quem quiser saber mais sobre Box (BoxLayout), indiquei o link do tutorial da Sun
+        //na descrição da classe.
+        Box boxHorizontalCima = Box.createHorizontalBox();
 
-	/*	 *//**
-			 * Retorna um container com as letras de cada linha do tabuleiro
-			 *
-			 * @return container - container com os labels
-			 */
-	/*
-	 * private Container criarVetorLetras() {
-	 *
-	 * Container container = new Container(); container.setPreferredSize(new
-	 * Dimension(10,250)); container.setLayout(new BoxLayout(container,
-	 * BoxLayout.Y_AXIS));
-	 *
-	 * JLabel label = null;
-	 *
-	 * for(char c = 'A'; c <= 'J'; c++) { label = new JLabel(c+"");
-	 * container.add(label); container.add(Box.createRigidArea(new
-	 * Dimension(0,9))); } return container; }
-	 *
-	 *//**
-		 * Retorna um container com os números de cada linha do tabuleiro
-		 *
-		 * @return container - container com os labels que indicam os números
-		 */
-	/*
-	 * private Container criarVetorNumeros() {
-	 *
-	 * Container container = new Container();
-	 *
-	 * container.setPreferredSize(new Dimension(250,10));
-	 * container.setLayout(new BoxLayout(container, BoxLayout.LINE_AXIS));
-	 *
-	 * JLabel label = null;
-	 *
-	 * container.add(Box.createRigidArea(new Dimension(30,0)));
-	 *
-	 * for(int i = 0; i < 10; i++) { label = new JLabel(i+"");
-	 * container.add(label); container.add(Box.createRigidArea(new
-	 * Dimension(18,0))); } return container; }
-	 */
+        //Aqui criamos 4 box auxiliares. A box1CimaX é uma box horizontal que adiciona o container com as letras e o tabuleiro do
+        //jogador. Depois "empacotamos" esta box na box1CimaY, junto com o container com os números. O mesmo acontece com box2CimaX
+        //e box2CimaY. Explicarei melhor no decorrer do código ;-)
+        Box box1CimaX = Box.createHorizontalBox(); Box box2CimaX = Box.createHorizontalBox();
+        Box box1CimaY = Box.createVerticalBox(); Box box2CimaY = Box.createVerticalBox();
+
+
+
+        Dimension distPadraoX = new Dimension(10, 249);
+        Dimension distPadraoY  = new Dimension(250,10);
+
+        //Criamos os containeres com as letras e números. bottom1 é um container que serve apenas para dar um espaço de preenchimento.
+        Container letras = criarVetorLetras();
+        Container numeros = criarVetorNumeros();
+        Container bottom1 = new Container();
+
+        bottom1.setPreferredSize(new Dimension(251,15));
+
+        //Primeiro adicionamos um espaço de preenchimento na box horizontal, com dimensão 10,252. Isto cria o espaço entre o lado
+        //esquerdo do frame e as letras
+        box1CimaX.add(new Box.Filler(distPadraoX,distPadraoX,distPadraoX));
+        //Adicionamos as letras  e o tabuleiro do jogador
+        box1CimaX.add(letras);
+       // box1CimaX.add(meuTabuleiro);
+
+        //Depois adicionamos um espaço de preenchimento similar à box horizontal, mas agora na vertical. Isto cria o espaço entre o
+        //a parte de cima do frame e os números
+        box1CimaY.add(new Box.Filler(distPadraoY,distPadraoY,distPadraoY));
+        //Adicionamos os números e a box horizontal, para empacotar tudo numa grande box
+        box1CimaY.add(numeros);
+        box1CimaY.add(box1CimaX);
+        //Adicionamos um espaço de preenchimento também na vertical. Isto cria o espaço entre o tabuleiro e o painel com os botões
+        //(ou dados da rede).
+        box1CimaY.add(new Box.Filler(distPadraoY,distPadraoY,distPadraoY));
+        //Por fim, adicionamos a box que contém o tabuleiro, letras e números na box horizontal de cima. Entenda que isto funciona
+        //basicamente como uma série de empacotamentos de containeres. Até aqui, adicionamos apenas o tabuleiro do jogador
+        boxHorizontalCima.add(box1CimaY);
+        boxHorizontalCima.add(new Box.Filler(distPadraoX,distPadraoX,distPadraoX));
+	}
+
+	 /**
+     * Retorna um container com as letras de cada linha do tabuleiro
+     *
+     * @return container - container com os labels
+     */
+    private Container criarVetorLetras() {
+
+        Container container = new Container();
+        container.setPreferredSize(new Dimension(10,250));
+        container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
+
+        JLabel label = null;
+
+        for(char c = 'A'; c <= 'J'; c++) {
+            label = new JLabel(c+"");
+            container.add(label);
+            container.add(Box.createRigidArea(new Dimension(0,9)));
+        }
+        return container;
+    }
+
+    /**
+     * Retorna um container com os números de cada linha do tabuleiro
+     * @return container - container com os labels que indicam os números
+     */
+    private Container criarVetorNumeros() {
+
+        Container container = new Container();
+
+        container.setPreferredSize(new Dimension(250,10));
+        container.setLayout(new BoxLayout(container, BoxLayout.LINE_AXIS));
+
+        JLabel label = null;
+
+        container.add(Box.createRigidArea(new Dimension(30,0)));
+
+        for(int i = 0; i < 10; i++) {
+            label = new JLabel(i+"");
+            container.add(label);
+            container.add(Box.createRigidArea(new Dimension(18,0)));
+        }
+        return container;
+    }
 
 	/**
 	 * This method initializes chatTextArea
@@ -411,7 +408,6 @@ public class TelaJogo extends JFrame {
 	private JTextArea getChatTextArea() {
 		if (chatTextArea == null) {
 			chatTextArea = new JTextArea();
-			chatTextArea.setBounds(new Rectangle(10, 13, 324, 155));
 		}
 		return chatTextArea;
 	}
@@ -424,20 +420,32 @@ public class TelaJogo extends JFrame {
 	private JTextField getInputTextField() {
 		if (inputTextField == null) {
 			inputTextField = new JTextField();
-			inputTextField.setBounds(new Rectangle(32, 178, 224, 20));
-			inputTextField
-					.addActionListener(new java.awt.event.ActionListener() {
-						public void actionPerformed(java.awt.event.ActionEvent e) {
-							Packet packet = new Packet("FROM", "TO", "CHAT",
-									inputTextField.getText());
-							output.SendPacket(packet);
-							chatTextArea
-									.append(inputTextField.getText() + "\n");
-							inputTextField.setText("");
-						}
-					});
+			inputTextField.setBounds(new Rectangle(12, 175, 325, 20));
+			inputTextField.addActionListener(new java.awt.event.ActionListener() {
+				public void actionPerformed(java.awt.event.ActionEvent e) {
+					if(!inputTextField.getText().trim().equals("") && inputTextField.getText()!=null){
+						output.SendPacket(new Packet("FROM","TO","CHAT",inputTextField.getText()));
+						chatTextArea.append(inputTextField.getText() + "\n");
+						inputTextField.setText("");
+					}
+				}
+			});
 		}
 		return inputTextField;
 	}
 
-} // @jve:decl-index=0:visual-constraint="10,17"
+	/**
+	 * This method initializes chatScrollPane
+	 *
+	 * @return javax.swing.JScrollPane
+	 */
+	private JScrollPane getChatScrollPane() {
+		if (chatScrollPane == null) {
+			chatScrollPane = new JScrollPane();
+			chatScrollPane.setBounds(new Rectangle(12, 16, 325, 156));
+			chatScrollPane.setViewportView(getChatTextArea());
+		}
+		return chatScrollPane;
+	}
+
+} // @jve:decl-index=0:visual-constraint="10,10"
